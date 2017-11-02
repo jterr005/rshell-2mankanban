48e278cb75974d4ac70a699b4625eb11f8849d0d
Moker(Ke) Bellomo <kmo004@ucr.edu>
2017-11-02 00:15:58 -0700

    tokenizer working but only 1 char delimeters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+//github.com/cs100/rshell-2mankanban.git
 #include <string>
 #include <boost/tokenizer.hpp>
+#include <boost/foreach.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/iter_find.hpp>
 
 using namespace std;
 using namespace boost;
@@ -11,14 +15,12 @@ int main(int, char**)
     cout << "$";
     getline(cin, input);
 
-    char_separator<char> sep("&&");
-    tokenizer<char_separator<char> > tokens(input, sep);
+    char_separator<char> sep("","|&;",boost::keep_empty_tokens);
+    tokenizer<char_separator<char> > tokens(input,sep);
 
-     for ( tokenizer<char_separator<char> >::iterator it = tokens.begin();
-         it != tokens.end();
-         ++it)
-   {
-      cout << *it << "." << endl;
+    for(tokenizer<char_separator<char> >::iterator it = tokens.begin();
+    it != tokens.end();++it){
+      cout << *it << endl;
    }
 }
 
