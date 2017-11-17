539bcafffc6e78b5247e6b1729631484ff3a41a7
Jason Terrazas <jterr005@hammer.cs.ucr.edu>
2017-11-17 01:46:47 -0800

    Created test cases for Test.cpp parsing

--- Test.cpp
+++ Test.cpp
@@ -11,9 +11,10 @@ string Test::type(){
 }
 void Test::execute(string cmd){
 	//Removes brackets from cmd
+	cout << "CMD BEFORE BRACKET REMOVAL: " << cmd << endl;
 	cmd.erase(std::remove(cmd.begin(), cmd.end(), '['), cmd.end());
 	cmd.erase(std::remove(cmd.begin(), cmd.end(), ']'), cmd.end());
-	
+	cout << "CMD AFTER BRACKET REMOVAL AND BEFORE SPACE REMOVAL: " << cmd << endl;
 	//Removes whitespace from cmd and parses cmd into vector
 	vector<string> strvctr;
 	boost::split(strvctr, cmd, boost::is_any_of(" "));
