37e77eb90616176b2128a634f009c1f79a231993
Jason Terrazas <jterr005@hammer.cs.ucr.edu>
2017-11-17 22:22:53 -0800

    Bugfixed test cases

--- src/Test.cpp
+++ src/Test.cpp
@@ -15,13 +15,15 @@ void Test::execute( string cmd ) {
 	vector<string> strvctr;
 
 	//cout << "This is the cmd for test: " << endl;
-
+	cout << "THIS IS CMD BEFORE REMOVING BRACKETS: " << cmd << endl;
 	//Removes brackets from cmd
 	cmd.erase( std::remove( cmd.begin(), cmd.end(), '[' ), cmd.end() );
 	cmd.erase( std::remove( cmd.begin(), cmd.end(), ']' ), cmd.end() );
+	cout << "THIS IS CMD AFTER REMOVING BRACKETS: " << cmd <<endl;
 	
 	//Removes whitespace from cmd and parses cmd into vector
 	boost::split( strvctr, cmd, boost::is_any_of( " " ) );
+	cout << "THIS IS STRVCTR.AT(0): " << strvctr.at(0) << endl;
 
 	//removes blank spaces and the word "test" that is pushed into 
 	//strvctr if this node is executed after the first cmd is run	
@@ -102,16 +104,30 @@ void Test::execute( string cmd ) {
 		}
 	}
 	else {
-		cout << "STRING AT AT(1)" << strvctr.at(0) << endl;
-		if(stat(strvctr.at(0).c_str(), &scan) == 0) { 
-			cout << "(True)" << endl;
-			this->set_evaluator(true);
-			return;
+		//cout << "STRING AT AT(1)" << strvctr.at(1) << endl;
+		if(strvctr.size() == 1) {
+			if(stat(strvctr.at(0).c_str(), &scan) == 0) { 
+				cout << "(True)" << endl;
+				this->set_evaluator(true);
+				return;
+			}
+			else {
+				cout << "(False)" << endl;
+				this->set_evaluator(false);
+				return;
+			}
 		}
 		else {
-			cout << "(False)" << endl;
-			this->set_evaluator(false);
-			return;
+			if(stat(strvctr.at(1).c_str(), &scan) == 0) {
+				cout << "(True)" << endl;
+				this->set_evaluator(true);
+				return;
+			}
+			else {
+				cout << "(False)" << endl;
+				this->set_evaluator(false);
+				return;
+			}
 		}
 	}
 }
