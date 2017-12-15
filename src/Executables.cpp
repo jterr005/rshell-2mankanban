2197aeeeab51b6058f84ef9dec74c5a7d237de8b
Jason Terrazas <jmterrazas008@gmail.com>
2017-12-14 20:32:36 -0800

    Cleaning up code

--- src/Executables.cpp
+++ src/Executables.cpp
@@ -16,36 +16,36 @@ string Executables::type() {
 }
 
 bool Executables::isParentRedirection() {
-	if(this->parent != NULL) {
-		if(parent->type() == "|") {
+	if (this->parent != NULL) {
+		if (parent->type() == "|") {
 			return true;
 		}
-		
-		if(parent->type() == "<") {
+
+		if (parent->type() == "<") {
 			return true;
 		}
-		
-		if(parent->type() == ">") {
+
+		if (parent->type() == ">") {
 			return true;
 		}
-		
-		if(parent->type() == ">>") {
+
+		if (parent->type() == ">>") {
 			return true;
 		}
-		
+
 		else {
 			return false;
 		}
 	}
-	
-	else { 
+
+	else {
 		return false;
 	}
 
 }
 
-void Executables::execute( string cmd ) {
-	if(isParentRedirection()) {
+void Executables::execute (string cmd) {
+	if (isParentRedirection()) {
 		return;
 	}
 
@@ -58,60 +58,60 @@ void Executables::execute( string cmd ) {
 	string tmp;
 
 	ss << cmd;
-	
+
 	//Determines size of char* array based on the number of strings in cmd
 	while ( ss >> tmp ) {
 		++size;
 	}
-	
+
 	//splits cmd and pushes individual strings into a vector
 	boost::split( strvctr, argmnt, boost::is_any_of(" ") );
-	
+
 	//checks if this current node is part of a sub-tree
 	if ( this->get_subTree() ) {
-		
+
 		//removes a blank space that is pushed into strvctr if this node is executed after the first cmd is run
 		if ( subTreeCounter > 1 ) {
 			strvctr.erase( strvctr.begin() );
 		}
-		
+
 		++subTreeCounter;
 	}
-	
+
 	else {
 		//removes a black space that is pushed into strvctr if this node is executed after the first cmd is run
 		if ( mainCounter > 1 ) {
-			strvctr.erase( strvctr.begin() );	
+			strvctr.erase( strvctr.begin() );
 		}
-		
-		++mainCounter;		
-	}	
-	
+
+		++mainCounter;
+	}
+
 	//Fills argmts array with cmd
 	while ( size > 0 ) {
 		argmts[ i ] = ( char* )strvctr.at( i ).c_str();
 		++i;
 		--size;
 	}
-	
+
 	argmts[ i ] = NULL;
 	int fds[2];
         int check;
-	
+
         check = pipe(fds);
-	
+
 	//begins actual processing of cmd
 	pid_t pid = fork();
-	
+
 	//cout << "THIS IS THE PIPE INT BITCH: " << check << endl;
-	
+
 	if(check == -1) {
 		perror("piping failed: ");
 		exit(1);
 	}
-	
-	//If a left child, executes command but instead of outputing 
-	//results to screen, it saves the output in a string that is 
+
+	//If a left child, executes command but instead of outputing
+	//results to screen, it saves the output in a string that is
 	//used as the input for this argument's parent's right child
 	if ( isParentRedirection() && this->parent->getLeftChild() == this  ) {
 		//cout << "ENTERED PIPE IF STATEMENT BOOIIIIIII" << endl;
@@ -131,14 +131,14 @@ void Executables::execute( string cmd ) {
 			perror("execvp failed: ");
 			exit(-1);
 		}
-		
+
 		else if (pid > 0) { //PARENT PROCESS
 			//cout << "about to do parent waitpid()" << endl;
 			if (waitpid(pid, &status, 0) == -1) {
 				perror("Wait: ");
 				this->success = false;
 			}
-			
+
 			if (WEXITSTATUS( status ) && WIFEXITED( status ) != 0 ) {
                                 this->success = false;
                         }
@@ -153,7 +153,7 @@ void Executables::execute( string cmd ) {
 				char buffer[4096];
 				int i = 0;
 				stringstream ss;
-				
+
 				//cout << "close() passed and now about to enter parent read()" << endl;
 				//Pushes READ part of pipe into parent connector's newInput string
 				while (read(fds[0], buffer, 4096) > 0) {
@@ -161,7 +161,7 @@ void Executables::execute( string cmd ) {
 					ss << buffer[i];
 					++i;
 				}
-				
+
 				string tempInput = ss.str();
 				this->parent->setNewInput(tempInput);
 				//cout << "THIS IS WHAT IS IN setNewInput: " << tempInput << endl;
@@ -171,42 +171,42 @@ void Executables::execute( string cmd ) {
 				return;
 
 			}
-			
+
 			return;
-	
+
 		}
-		 
+
 		else if ( pid < 0 ) {
                         return;
                 }
 
 	}
-	
+
 	else {
 		if ( pid == 0 ) { //CHILD PROCESS
 			execvp ( argmts[ 0 ], argmts );
 			perror ( "execvp failed: " );
 			exit ( -1 );
 		}
-	
-		else if ( pid > 0 ) { //PARENT PROCESS	
+
+		else if ( pid > 0 ) { //PARENT PROCESS
 			if ( waitpid( pid, &status, 0 ) == -1 ) {
 				perror( "Wait: " );
 				this->success = false;
 			}
-			
+
 			if ( WEXITSTATUS( status ) && WIFEXITED( status ) != 0 ) {
 				this->success = false;
 			}
-		
+
 			else { //if the process was successful, this node's success is set to true
                 		this->success = true;
                 		return;
-        		}	
-			
+        		}
+
 			return;
 		}
-		
+
 		else if ( pid < 0 ) {
 			return;
 		}
@@ -214,4 +214,3 @@ void Executables::execute( string cmd ) {
 	}
 
 }
-
